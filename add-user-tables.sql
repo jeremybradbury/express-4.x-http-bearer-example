555eff25d1b195771556972eb771563f48e27584
Jeremy Bradbury <jbradbury@connectivedx.com>
2017-05-21 22:22:56 -0700

    add demo user

--- add-user-tables.sql
+++ add-user-tables.sql
@@ -22,6 +22,15 @@ CREATE TABLE IF NOT EXISTS  `restful_api_demo`.`user_tokens` (
   CONSTRAINT `user_info_foreign_key` FOREIGN KEY (`user_id_fk`) REFERENCES `users` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+LOCK TABLES `user_tokens` WRITE;
+/*!40000 ALTER TABLE `user_tokens` DISABLE KEYS */;
+
+INSERT INTO `user_tokens` (`user_info_id`, `user_id_fk`, `token`)
+VALUES (1,1,'123456789');
+
+/*!40000 ALTER TABLE `user_tokens` ENABLE KEYS */;
+UNLOCK TABLES;
+
 
 # table users
 # ------------------------------------------------------------
@@ -34,6 +43,15 @@ CREATE TABLE IF NOT EXISTS  `restful_api_demo`.`users` (
   PRIMARY KEY (`id`),
   UNIQUE KEY `user_email_UNIQUE` (`email`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+
+INSERT INTO `users` (`id`, `email`, `password`, `join_date`)
+VALUES (1,'jeremy@example.com','941f8c79b34be1dba86024719b47718e','2000-01-01 00:00:01');
+
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
 
 
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
