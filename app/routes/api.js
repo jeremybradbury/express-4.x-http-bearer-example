37367b2e1295fed4df1b9839c99b43d90839f5bb
Jeremy Bradbury <jdbradbury@gmail.com>
2017-05-23 17:54:29 -0700

    #3 & #4 create route subfolders: one route file for each Object/Table

--- app/routes/api.js
+++ app/routes/api.js
@@ -22,7 +22,7 @@ function passwordReset(req, res, next, connection, md5, app) {
 }
 // TODO: POST `/api/request-token` endpoint required: email/password
 REST_ROUTER.prototype.handleRoutes = (router,connection,md5,app) => {
-  // api routes
+  // API home routes
   router.route("/")
     .get((req, res) => {
       var msg = {
@@ -48,74 +48,8 @@ REST_ROUTER.prototype.handleRoutes = (router,connection,md5,app) => {
       };
       res.json({ Error: false, Message: msg });
     });
-  router.route("/users")
-    .get((req, res, next) => { // Index Users (no limit set... yet)
-      var query = "SELECT ??, ?? FROM ??";
-      var table = ["id","email","users"];
-      query = mysql.format(query,table);
-      connection.query(query,function(err,rows){
-        if(err) {
-          meJSON = {"Error" : true, "Message" : "Error executing MySQL query. "};
-          res.json(meJSON);
-          app.errorLogger.error(meJSON.Message+err);
-        } else {
-          meJSON = {"Error" : false, "Message" : "Success", "Users" : rows};
-          res.json(meJSON);
-          app.errorLogger.info(meJSON.Message,rows);
-        }
-      });
-    })
-    .post((req, res, next) => { // Create new User
-      var query = "INSERT INTO ??(??,??) VALUES (?,?)";
-      var table = ["users","email","password",req.body.email,md5(req.body.password)];
-      query = mysql.format(query,table);
-      connection.query(query,function(err,rows){
-          if(err) {
-            meJSON = {"Error" : true, "Message" : "Error executing MySQL query. "};
-            res.json(meJSON);
-            app.errorLogger.error(meJSON.Message+err);
-          } else {
-            meJSON = {"Error" : false, "Message" : "User Added !"};
-            res.json(meJSON);
-            app.errorLogger.info(meJSON.Message);
-          }
-      });
-    })
-    .put((req, res, next) => { passwordReset(req,res,next,connection,md5,app) }); // Update password by email Documentation
-  router.route("/user/:id")
-    .get((req, res, next) => { // Read User by id Documentation
-      var query = "SELECT * FROM ?? WHERE ??=?";
-      var table = ["users","user_id",req.params.id];
-      query = mysql.format(query,table);
-      connection.query(query,function(err,rows){
-        if(err) {
-          meJSON = {"Error" : true, "Message" : "Error executing MySQL query. "};
-          app.errorLogger.err(meJSON.Message+err);
-          res.json(meJSON);
-        } else {
-          meJSON = {"Error" : false, "Message" : "Success", "Users" : rows};
-          app.errorLogger.info(meJSON);
-          res.json(meJSON);
-        }
-      });
-    })
-    .delete((req, res, next) => { // Delete User by id Documentation
-      var query = "DELETE from ?? WHERE ??=?";
-      var table = ["users","user_id",req.params.id];
-      query = mysql.format(query,table);
-      connection.query(query,function(err,rows){
-        if(err) {
-          meJSON = {"Error" : true, "Message" : "Error executing MySQL query. "};
-          res.json(meJSON);
-          app.errorLogger.error(meJSON.Message+err);
-        } else {
-          meJSON = {"Error" : false, "Message" : "Deleted the user "+req.params.id};
-          res.json(meJSON);
-          app.errorLogger.info(meJSON.Message);
-        }
-      });
-    });
-  router.route("/password-reset")
-    .put((req, res, next) => { passwordReset(req,res,next,connection,md5,app) }); // Update password by email Documentation
+  // User routes
+  var userRoutes = require("./api/users")(router,connection,md5,app);
+  
 }
 module.exports = REST_ROUTER;
--- app/routes/docs.js
+++ app/routes/docs.js
@@ -3,7 +3,7 @@ function REST_ROUTER(router,connection,md5,app) {
   self.handleRoutes(router,connection,md5,app);
 }
 REST_ROUTER.prototype.handleRoutes = (router,connection,md5,app) => {
-  // docs routes
+  // Docs home routes
   router.route("/")
     .get((req, res) => {
       var msg = {
@@ -29,75 +29,7 @@ REST_ROUTER.prototype.handleRoutes = (router,connection,md5,app) => {
     };
     res.json({ Error: false, Message: msg });
     });
-  // TODO: paging for User records
-  router.route("/users")
-    .get((req, res, next) => { // Index Users
-      var msg = {
-        description: "This is the User Index. It lists all users without limiting user count... yet.",
-        endpoint: "[GET: " + app.baseUrl + "/api/users/]",
-        type: "Index"
-      };
-      res.json({ Error: false, Message: msg });
-    })
-    .post((req, res, next) => { // Create new User
-      var msg = {
-        description: "This Creates a single record. All body parameters are optional and only the data provided is added. If nothing is provided only the id & created date are set.",
-        endpoint: "[POST: " + app.baseUrl + "/api/users/]",
-        type: "Create",
-        postBodyParameters: {
-          email:   { required: false, type: "string",  sample: "jeremy@example.com" },
-          password:{ required: false, type: "string",  sample: "This is not actually a sample password, do not use it." },
-        }
-      };
-      res.json({ Error: false, Message: msg });
-    })
-    .put((req, res, next) => { // Update User's password by email Documentation
-      var msg = {
-        description: "Updates a User's password by email record. Both body parameters are required, only the password for this record can/will change.",
-        endpoint: "[PUT: " + app.baseUrl + "/api/users/]",
-        type: "Create",
-        postBodyParameters: {
-          email:   { required: true, type: "string",  sample: "jeremy@example.com" },
-          password:{ required: true, type: "string",  sample: "This is not really a sample password, do not use it." },
-        }
-      };
-      res.json({ Error: false, Message: msg });
-    });
-  router.route("/user/:id")
-    .get((req, res, next) => { // Read User by id Documentation
-      var msg = {
-        description: 'Single User lookup and Read.',
-        endpoint: '[GET: ' + app.baseUrl + '/api/user/:id]',
-        type: 'Read',
-        getParameters: {
-            id: { required: true, type: 'integer', sample: 2080 }
-        }
-      };
-      res.json({ Error: false, Message: msg });
-    })
-    .delete((req, res, next) => { // Delete User by id Documentation
-      var msg = {
-        description: 'This is a single user delete.',
-        endpoint: '[DELETE: ' + app.baseUrl + '/api/user/:id]',
-        type: 'Delete',
-        getParameters: {
-          id: { required: true, type: 'integer', sample: 2080 }
-        }
-      };
-      res.json({ Error: false, Message: msg });
-    });
-  router.route("/password-reset")
-    .put((req, res, next) => { // Update password by email Documentation
-      var msg = {
-            description: "This Updates a user's password by email record. Both body parameters are required, only the password for this record can change.",
-            endpoint: "[POST: " + app.baseUrl + "/api/users/]",
-            type: "Create",
-            postBodyParameters: {
-                email:   { required: true, type: "string",  sample: "jeremy@example.com" },
-                password:{ required: true, type: "string",  sample: "This is not really a sample password, do not use it." },
-            }
-        };
-        res.json({ Error: false, Message: msg });
-	  });
+  // User routes
+  var userRoutes = require("./docs/users")(router,connection,md5,app);
 }
 module.exports = REST_ROUTER;
