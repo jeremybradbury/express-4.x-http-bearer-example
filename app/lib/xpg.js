7e98fa5ebce11556f30a0934e6aa75a6ff51da91
Jeremy Bradbury <jdbradbury@gmail.com>
2017-05-29 21:34:02 -0700

    add comments and change our default dictionary

--- app/lib/xpg.js
+++ app/lib/xpg.js
@@ -1,7 +1,10 @@
 var generate = require('xkcd-pass-plus');
 module.exports = function(){
   var options = { 
-    words: { dictionary: 'mixed', num: 4, min: 4, max: 8 },
+    words: { dictionary: 'xkcd', num: 4, min: 4, max: 8 },  // dictionary:
+    // `xkcd` (2k memorable words ~44 bits entropy) 
+    // `letterpress` (270k words >60 bits entropy) or 
+    // `mixed` (default, 272k words ~80 bits entropy) 
     separator: ' ',
     paddingDigits: { after: 0 },
     paddingSymbols: { after: 0 }
